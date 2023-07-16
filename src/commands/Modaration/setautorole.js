a88eee50a8a6723accfb33fbe185905ee1303163
norkart1 <138743746+norkart1@users.noreply.github.com>
2023-07-16 05:53:22 +0000

    05

--- src/commands/Modaration/setautorole.js
+++ src/commands/Modaration/setautorole.js
@@ -1,29 +1,3 @@
-/*
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { PermissionsBitField, EmbedBuilder } = require('discord.js');
-const { QuickDB } = require('quickdb');
-
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('setautorole')
-    .setDescription('This sets a auto join role ')
-    .addRoleOption(option => option.setName('role').setDescription('This is the role you want as as your autorole ').setRequired(true)),
-
-  async execute(interaction) {
-
-    if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return await interaction.reply({ content: 'You cannot set an auto role role ', ephemeral: true });
-
-    const role = interaction.options.getRole('role');
-
-    await db.set(`autorole_${interaction.guild.id}`, role.id);
-
-    const embed = new EmbedBuilder()
-      .setColor('Blue')
-      .setDescription(`:white_check_mark: Your auto role has been set to ${role}`)
-
-    await interaction.reply({ embeds: [embed] });
-  }
-}*/
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { PermissionsBitField, EmbedBuilder } = require('discord.js');
 const { QuickDB } = require('quick.db');
